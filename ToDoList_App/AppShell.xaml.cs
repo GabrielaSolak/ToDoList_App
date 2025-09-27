3ca937a40e61667b7981999fff9c95b78124939b
142ga <142ga@LAPTOP-O15SPMSA>
2025-09-27 12:22:59 +0200

    Dodaj pliki projektów.

---
+++ ToDoList_App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ToDoList_App/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ToDoList_App
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Satoshi-Bold.otf", "SatoshiBold");
+                    fonts.AddFont("Satoshi-Variable.ttf", "SatoshiVariable");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
