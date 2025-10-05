d5866ef2f31f6b28b93d465b5741dcafb9c9f3e5
Gabriela Solak <gabrielasolak00@gmail.com>
2025-10-05 21:22:18 +0200

    dodawanie zadań

--- ToDoList_App/MainPage.xaml.cs
+++ ToDoList_App/MainPage.xaml.cs
@@ -1,18 +1,30 @@
-﻿namespace ToDoList_App
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoList_App
 {
     public partial class MainPage : ContentPage
     {
-
+        public ObservableCollection<Zadanie> Zadania { get; set; } = new();
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
         private void dodaj_zadanie(object sender, EventArgs e)
         {
-            
+            if (!string.IsNullOrWhiteSpace(nowe_zadanie.Text))
+            {
+                Zadania.Add(new Zadanie { Name = nowe_zadanie.Text });
+                nowe_zadanie.Text = string.Empty;
+            }
         }
 
+    }
+
+    public class Zadanie
+    {
+        public string Name { get; set; }
     }
 
 }
